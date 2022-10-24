83573deffb9ec166144cf30b8c673b1e832f18cc
PawelekPro <pawel.gilewicz1@gmail.com>
2022-10-24 20:01:55 +0200

    pierwszy commit testowy wstepny szkielet pliku wektor2d

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,4 +1,44 @@
+#include <iostream>
+using namespace std;
+
 class Wektor2D
 {
-    // Tutaj napisz implementacje klasy Wektor2D
+private:
+    double X;
+    double Y;
+
+public:
+    Wektor2D(double x = 0., double y = 0.)
+    {
+        X = x;
+        Y = y;
+    };
+    Wektor2D(double xx, double yy)
+    {
+        x = xx;
+        y = yy;
+    };
+    
+    double getX() { return X; };
+    double getY() { return Y; };
+
+
+    void   setX(double x) { X = x; };
+    void   setY(double y) { Y = y; };
+
+    
+
+    Wektor2D operator+(const Wektor2D &v1)
+    {
+        Wektor2D sum;
+        sum.X = X + v1.X;
+        sum.Y = Y + v1.Y;
+        return sum;
+    };
+
+    double operator*(const Wektor2D &v1) 
+    {
+        return X * v1.X + Y * v1.Y; 
+    };
+
 };
